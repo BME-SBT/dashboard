909ab567200a4c307163ca35d46a0fcf0081c9fc
Erik674 <79448116+Erik674@users.noreply.github.com>
2023-02-18 19:22:19 +0100

    1. próba

---
+++ Bits_To_Decimal.py
@@ -0,0 +1,26 @@
+import math
+
+def CAN_To_Decimal(data, length, sign, res):
+    value = 0
+    if (sign == 0 | ((sign == 1) & (int(data[0]) == 0))):
+        for i in range(length):
+            value = value + int(data[length-1-i])*(2**i)
+    else:
+        carry = 1
+        for i in range(length):
+            if (int(not(int(data[length-1-i])))==1):
+                if (carry == 0):
+                    value = value + (2**i)
+            else:
+                if (carry == 1):
+                    value = value + (2**i)
+                    carry = 0
+        value = value * (-1)
+    
+    return math.floor(value*res*10)/10
+
+    
+    
+    
+    
+print(CAN_To_Decimal("1000000000000000",16,1,0.1))
