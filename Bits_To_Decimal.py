6ace9e51cd29716667dda40be5fea678c0ed97d1
Brályen <envagyok321@gmail.com>
2023-02-26 15:15:30 +0100

    kuttya

--- Bits_To_Decimal.py
+++ Bits_To_Decimal.py
@@ -1,14 +1,19 @@
 import math
 
-def CAN_To_Decimal(data, length, sign, res):
+def CAN_To_Decimal(data, signed, res):
     value = 0
-    if (sign == 0 | ((sign == 1) & (int(data[0]) == 0))):
+    length = 0
+    for i in range(4):
+        length = length + int(data[19-i])*(2**i)
+    length = length * 8
+    
+    if (signed == 0 | ((signed == 1) & (int(data[20]) == 0))):
         for i in range(length):
-            value = value + int(data[length-1-i])*(2**i)
+            value = value + int(data[19+length-i])*(2**i)
     else:
         carry = 1
         for i in range(length):
-            if (int(not(int(data[length-1-i])))==1):
+            if (int(not(int(data[19+length-i])))==1):
                 if (carry == 0):
                     value = value + (2**i)
             else:
@@ -17,10 +22,15 @@ def CAN_To_Decimal(data, length, sign, res):
                     carry = 0
         value = value * (-1)
     
-    return math.floor(value*res*10)/10
-
+    if (res == 1):
+        return value
+    else:
+        return math.floor(value*res*10)/10
     
     
     
-#asdasd  
-print(CAN_To_Decimal("1000000000000000",16,1,0.1))
+#                                      |  |
+#             Data length code (2byte) v  v
+#                                          |              ||               |
+#                                          v  Data(16bit) vv    maradék    v
+print( CAN_To_Decimal("00000000000000000010100000000000000022222222222222222",1,1))
