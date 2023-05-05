6409b2a106f6784e106be726e4bed84911c90740
hegeda <hegeda114@gmail.com>
2023-05-05 23:37:03 +0200

    Implement battery_temperature_widget placeholder version.

--- gui/view/panel_elements/battery_temperature_widget.py
+++ gui/view/panel_elements/battery_temperature_widget.py
@@ -1,32 +1,32 @@
-from PySide6.QtCore import Signal
-from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QLabel, QVBoxLayout
+
+from data.sensor import SensorState
+from gui.view.panel_elements.abstract_panel_elements.abstract_panel_element import AbstractPanelElement
 
 
-class BatteryTemperatureWidget(QWidget):
-    # SIG_CHANGED = Signal(int)
-
-    def __init__(self, sensor_id):
-        super().__init__()
-
-        self.sensor_id = 111111
-        # self.sensor = SensorManager.get_sensor(sensor_id)
-        #
-        # self.sensor.register_callback(
-        #     lambda val: self.sig_valami.emit(val)
-        # )
+class BatteryTemperatureWidget(AbstractPanelElement):
+    # TODO implement custom design
+    def __init__(self):
+        super().__init__(title='Battery Temperature', sensor_id=0b00010010000)
 
         # Create main layout
         main_layout = QVBoxLayout(self)
 
         # Panel name
-        name_label = QLabel('BatTemp')
+        name_label = QLabel(self.title)
         main_layout.addWidget(name_label)
 
         # Panel value
         self.value_label = QLabel('0')
         main_layout.addWidget(self.value_label)
 
-        # Create connections
-        # SIG_MANAGER.sig_battery_temperature_changed.connect(
-        #     lambda value: self.value_label.setText(str(value))
-        # )
+    def sensor_state_changed(self, state, old_state):
+        if state == SensorState.MISSING_DATA:
+            self.value_label.setStyleSheet("color: red")
+            # TODO (dani) replace setStyleSheet to setProperty
+            self.value_label.setText(self.value_label.text() + " - NO DATA")
+        else:
+            self.value_label.setStyleSheet("color: black")
+
+    def sensor_value_changed(self, value, old_value):
+        self.value_label.setText(f"{value} {self.sensor.data_type.unit}")
