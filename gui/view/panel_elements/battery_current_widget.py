fdc2088c9ad9d43511ca80dac64318edfb4bc726
hegeda <hegeda114@gmail.com>
2023-05-05 23:37:36 +0200

    Implement battery_current_widget placeholder version.

--- gui/view/panel_elements/battery_current_widget.py
+++ gui/view/panel_elements/battery_current_widget.py
@@ -1,22 +1,32 @@
-from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QLabel, QVBoxLayout
+
+from data.sensor import SensorState
+from gui.view.panel_elements.abstract_panel_elements.abstract_panel_element import AbstractPanelElement
 
 
-class BatteryCurrentWidget(QWidget):
+class BatteryCurrentWidget(AbstractPanelElement):
+    # TODO implement custom design
     def __init__(self):
-        super().__init__()
+        super().__init__(title='Battery Current', sensor_id=0b01001010000)
 
         # Create main layout
         main_layout = QVBoxLayout(self)
 
         # Panel name
-        name_label = QLabel('BatCurr')
+        name_label = QLabel(self.title)
         main_layout.addWidget(name_label)
 
         # Panel value
         self.value_label = QLabel('0')
         main_layout.addWidget(self.value_label)
 
-        # Create connections
-        # SIG_MANAGER.sig_battery_current_changed.connect(
-        #     lambda value: self.value_label.setText(str(value))
-        # )
+    def sensor_state_changed(self, state, old_state):
+        if state == SensorState.MISSING_DATA:
+            self.value_label.setStyleSheet("color: red")
+            # TODO (dani) replace setStyleSheet to setProperty
+            self.value_label.setText(self.value_label.text() + " - NO DATA")
+        else:
+            self.value_label.setStyleSheet("color: black")
+
+    def sensor_value_changed(self, value, old_value):
+        self.value_label.setText(f"{value} {self.sensor.data_type.unit}")
