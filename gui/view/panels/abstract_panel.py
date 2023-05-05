373268c5683ca8aa9e918a1a8b0c2fd7fcd22de3
hegeda <hegeda114@gmail.com>
2023-05-05 22:03:25 +0200

    Remove title and panel_layout from abstract_panel.

--- gui/view/panels/abstract_panel.py
+++ gui/view/panels/abstract_panel.py
@@ -1,28 +1,11 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PySide6.QtWidgets import QWidget
 
 
 class AbstractPanel(QWidget):
-    def __init__(self, title=''):
+    def __init__(self):
         super().__init__()
 
         # Style
         self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground)
         self.setFixedSize(850, 400)
-
-        # Layout
-        main_layout = QVBoxLayout(self)
-        main_layout.setContentsMargins(0, 0, 0, 0)
-
-        # Title
-        title_label = QLabel()
-        title_label.setProperty('class', 'TitleLabel')
-        title_label.setText(title)
-
-        # Panel container
-        self.panel_container = QVBoxLayout()
-
-        # Add widgets
-        main_layout.addWidget(title_label, alignment=Qt.AlignmentFlag.AlignCenter)
-        main_layout.addStretch()
-        main_layout.addLayout(self.panel_container)
