059d289735963d64e2444c1f14532c764293f83e
hegeda <hegeda114@gmail.com>
2023-05-05 23:28:27 +0200

    Create empty home panel.

---
+++ gui/view/panels/home_panel.py
@@ -0,0 +1,15 @@
+from gui.view.panels.abstract_panel import AbstractPanel
+
+
+class HomePanel(AbstractPanel):
+    def __init__(self):
+        super().__init__()
+
+        # TODO (sari) implement home panel layout here. Panel elements:
+        #   - RPM - MotorRPMWidget
+        #   - Sebesseg - GPSSpeedWidget
+        #   - Akkufesz - BatteryVoltageWidget
+        #   - Motor telj - Motor...
+        #   - Motor ho - MotorTemperatureWidget
+        #   - Akku ho - BatteryTemperatureWidget
+        #   - Akku aram - BatteryCurrentWidget
